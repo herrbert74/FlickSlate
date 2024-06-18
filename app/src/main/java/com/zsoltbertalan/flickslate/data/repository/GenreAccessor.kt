49c65a9410e230272a08f779e88313971f587a53
Zsolt Bertalan <herrbert74@gmail.com>
2024-06-18 18:09:45 +0100

    Remove suspend from Flow returning functions, rethrow CancellationException in runCatching

--- app/src/main/java/com/zsoltbertalan/flickslate/data/repository/GenreAccessor.kt
+++ app/src/main/java/com/zsoltbertalan/flickslate/data/repository/GenreAccessor.kt
@@ -6,14 +6,15 @@ import com.zsoltbertalan.flickslate.data.network.FlickSlateService
 import com.zsoltbertalan.flickslate.data.network.dto.GenreResponse
 import com.zsoltbertalan.flickslate.data.network.dto.toGenres
 import com.zsoltbertalan.flickslate.data.network.dto.toMoviesResponse
+import com.zsoltbertalan.flickslate.di.IoDispatcher
 import com.zsoltbertalan.flickslate.domain.api.GenreRepository
 import com.zsoltbertalan.flickslate.domain.model.Genre
 import com.zsoltbertalan.flickslate.ext.ApiResult
 import com.zsoltbertalan.flickslate.ext.apiRunCatching
 import com.zsoltbertalan.flickslate.util.getresult.fetchCacheThenNetworkResponse
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -21,25 +22,24 @@ import javax.inject.Singleton
 class GenreAccessor @Inject constructor(
 	private val flickSlateService: FlickSlateService,
 	private val genreDataSource: GenreDataSource,
+	@IoDispatcher val dispatcher: CoroutineDispatcher
 ) : GenreRepository {
 
-	override suspend fun getGenresList(): Flow<ApiResult<List<Genre>>> {
-		return withContext(Dispatchers.IO) {
-			fetchCacheThenNetworkResponse(
-				fetchFromLocal = { genreDataSource.getGenres() },
-				makeNetworkRequest = {
-					val etag = genreDataSource.getEtag()
-					flickSlateService.getGenres(ifNoneMatch = etag)
-				},
-				saveResponseData = { genreResponse ->
-					val etag = genreResponse.headers()["etag"] ?: ""
-					genreDataSource.insertEtag(etag)
-					val genres = genreResponse.body()?.toGenres().orEmpty()
-					genreDataSource.insertGenres(genres)
-				},
-				mapper = GenreResponse::toGenres,
-			)
-		}
+	override fun getGenresList(): Flow<ApiResult<List<Genre>>> {
+		return fetchCacheThenNetworkResponse(
+			fetchFromLocal = { genreDataSource.getGenres() },
+			makeNetworkRequest = {
+				val etag = genreDataSource.getEtag()
+				flickSlateService.getGenres(ifNoneMatch = etag)
+			},
+			saveResponseData = { genreResponse ->
+				val etag = genreResponse.headers()["etag"] ?: ""
+				genreDataSource.insertEtag(etag)
+				val genres = genreResponse.body()?.toGenres().orEmpty()
+				genreDataSource.insertGenres(genres)
+			},
+			mapper = GenreResponse::toGenres,
+		).flowOn(dispatcher)
 	}
 
 	override fun getGenreDetail(
--- app/src/main/java/com/zsoltbertalan/flickslate/ext/KotlinResult.kt
+++ app/src/main/java/com/zsoltbertalan/flickslate/ext/KotlinResult.kt
@@ -1,8 +1,11 @@
 package com.zsoltbertalan.flickslate.ext
 
 import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.onFailure
 import com.github.michaelbull.result.runCatching
+import kotlin.coroutines.cancellation.CancellationException
 
 typealias ApiResult<T> = Result<T, Throwable>
 
 inline fun <T, V> T.apiRunCatching(block: T.() -> V) = runCatching(block)
+	.onFailure { if (it is CancellationException) throw it }
